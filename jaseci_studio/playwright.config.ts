1908a5dec999bb692f88c6526a07198b88914034
Gimel Dick <gimeldick@gmail.com>
2023-01-20 09:15:08 -0400

    update config

--- jaseci_studio/playwright.config.ts
+++ jaseci_studio/playwright.config.ts
@@ -42,7 +42,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:8007",
+    // baseURL: "http://localhost:8007",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -64,12 +64,12 @@ const config: PlaywrightTestConfig = {
       },
     },
 
-    {
-      name: "webkit",
-      use: {
-        ...devices["Desktop Safari"],
-      },
-    },
+    // {
+    //   name: "webkit",
+    //   use: {
+    //     ...devices["Desktop Safari"],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
